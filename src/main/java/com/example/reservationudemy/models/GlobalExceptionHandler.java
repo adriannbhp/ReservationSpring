2695e02a7fc0e08d17ad988c8a6717541a58d443
adriannbhp <adrianbimo43@gmail.com>
2024-01-04 16:53:44 +0700

    add Exception Handler

---
+++ src/main/java/com/example/reservationudemy/models/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.example.reservationudemy.models;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.nio.file.AccessDeniedException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ReservationApiException.class)
+    public ResponseEntity<ErrorDetails> handleReservationApiException(
+            ReservationApiException exception,
+            WebRequest request
+    ) {
+        final ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setErrorCode(exception.getStatus().value());
+        errorDetails.setErrorMessage(exception.getLocalizedMessage());
+        errorDetails.setDevErrorMessage(request.getDescription(false));
+        errorDetails.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDetails> handleAccessDeniedException(
+            ReservationApiException exception,
+            WebRequest request
+    ) {
+        final ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setErrorCode(exception.getStatus().value());
+        errorDetails.setErrorMessage(exception.getLocalizedMessage());
+        errorDetails.setDevErrorMessage(request.getDescription(false));
+        errorDetails.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+}
